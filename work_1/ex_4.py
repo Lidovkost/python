af0fa289930aa31d512df25caed866c37e12397c
«Lidovskiy_Konstantin» <lidovkost@gmail.com>
2023-02-03 15:28:39 +0300

    исправление замечания по ex_4

--- work_1/ex_4.py
+++ work_1/ex_4.py
@@ -3,6 +3,7 @@ text_profit_of_revenue = 'Рентабельность выручки = '
 text_workers_count = 'Введите численность сотрудников фирмы: '
 text_profit_per_worker = 'Прибыль фирмы в расчете на одного сотрудника = '
 text_loss = 'Финансовый результат - убыток. Его величина:'
+text_profit_zero = 'выручка равна издержкам, прибыль = 0'
 
 revenue = float(input('Введите выручку фирмы: '))
 costs = float(input('Введите издержки фирмы: '))
@@ -13,5 +14,8 @@ if fin_result > 0:
     print(text_profit_of_revenue,fin_result/revenue)
     workers_count = int(input(text_workers_count))
     print(text_profit_per_worker,fin_result/workers_count)
-else: print(text_loss,abs(fin_result))
+elif fin_result < 0:
+    print(text_loss,abs(fin_result))
+else:
+    print(text_profit_zero)
 
