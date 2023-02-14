3bdc26bc8a040f30e8979db64d7197b9aa762387
«Lidovskiy_Konstantin» <lidovkost@gmail.com>
2023-02-14 07:16:29 +0300

    added work_5/ex_1-7

---
+++ work_5/ex_3.py
@@ -0,0 +1,12 @@
+def reverse_number(int_number): 
+    if len(str(int_number)) == 1:
+        return int_number 
+    return str(int_number%10) + str(reverse_number(int_number//10))
+def input_number():
+    try:
+        return int(input('введите число '))
+    except:
+        print('вы ввели не число')
+        return input_number()
+a = input_number()
+print(reverse_number(a))
---
+++ work_5/ex_7.py
@@ -0,0 +1,19 @@
+def num_input(text):
+    number = input(text)
+    try:
+        return int(number)
+    except:
+        print('вы ввели не число(( ')
+        return num_input(text) 
+number = num_input('Введите число, n = ')
+def sum_arithmetic_progression(n):
+    if  n == 1:
+        return n
+    return n + sum_arithmetic_progression(n-1)
+def gauss_formul_for_arithmetic_progress(n):
+    return int(n*(n+1)/2)
+number1 = sum_arithmetic_progression(number)
+number2 = gauss_formul_for_arithmetic_progress(number)
+if number1 == number2:
+    print('для множества натуральных чисел выполняется равенство: 1+2+...+n = n(n+1)/2, где n - любое натуральное число.')
+    
